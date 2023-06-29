a5e51c72282cb211a7ad54446bbfc23825a6687f
RCJK33 <rafa.11.rcj@gmail.com>
2023-06-29 06:07:06 -0700

    Class 4
    Task Manager fron end made with flask and Jinja python frameworks

---
+++ app/routes.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+
+BASE_URL = "http://127.0.0.1:5000/tasks"
+
+""" @app.get('/<name>')
+def index(name):
+    out = ""
+    for i in range(1, 7):
+        out += f"<h{i}>Hello, {name}!</h{i}>\n"
+    return out """
+
+
+@app.get('/')
+def main():
+    timestamp = datetime.now().strftime("%F %H:%M:%S")
+    return render_template('home.html', ts=timestamp)
+
+
+@app.get('/about')
+def about_me():
+    return render_template('about.html')
+
+@app.get('/tasks')
+def display_all_tasks():
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        task_list = response.json().get('tasks')
+        return render_template('list.html', tasks=task_list)
+    return render_template('error.html', err=response.status_code), response.status_code
+    
+@app.get('/tasks/<int:task_id>')
+def display_task(task_id):
+    response = requests.get(f"{BASE_URL}/{task_id}")
+    if response.status_code == 200:
+        task = response.json().get('task')
+        return render_template('task.html', task=task)
+    return render_template('error.html', err=response.status_code), response.status_code
+
+@app.get('/tasks/new')
+def new_task_form():
+    return render_template('new.html')
+
+@app.post('/tasks')
+def create_task():
+    task_data = request.form
+    task_json = {
+        "summary": task_data.get('summary'),
+        "description": task_data.get('description'),
+    }
+    response = requests.post(BASE_URL, json=task_json)
+    if response.status_code == 201:
+        return render_template('success.html', task=response.json().get('task'), message='New task created!')
+    return render_template('error.html', err=response.status_code), response.status_code
